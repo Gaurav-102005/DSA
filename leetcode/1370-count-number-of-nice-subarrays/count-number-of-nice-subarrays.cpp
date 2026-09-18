08b7831636abfcd11f761a8f04f16df4340908e3
Gaurav-102005 <gauravbhasker10@gmail.com>
2026-09-18 12:18:21 +0530

    Time: 6 ms (70.62%) | Memory: 71.6 MB (47.37%) - LeetSync

--- leetcode/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ leetcode/1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -4,14 +4,12 @@ public:
         if(g < 0) return 0;
         int l = 0;
         int r = 0;
-        int odd = 0;
+        int sum = 0;
         int cnt = 0;
         while(r < n.size()) {
-            if(n[r] % 2 != 0) {
-                odd++;
-            }
-            while(odd > g) {
-                if(n[l]% 2 != 0) odd--;
+            sum += n[r]%2;
+            while(sum > g) {
+                sum -= n[l]%2;
                 l++;
             }
             cnt += (r-l+1);
@@ -19,6 +17,7 @@ public:
         }
         return cnt;
     }
+
     int numberOfSubarrays(vector<int>& nums, int k) {
 
         int ans = cntSub(nums, k) - cntSub(nums, k-1);
