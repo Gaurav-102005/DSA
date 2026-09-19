a8f6c4b5156f25ead4f1e74b688bd98cbc771902
Gaurav-102005 <gauravbhasker10@gmail.com>
2026-09-19 18:59:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (27.12%) - LeetSync

---
+++ leetcode/76-minimum-window-substring/minimum-window-substring.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+        int hash[256] = {0};
+
+        int l = 0;
+        int r = 0;
+        int minLen = 1e9;
+        int sIndex = -1;
+        int cnt = 0;
+
+        for(int i = 0; i < m; i++) {
+            hash[t[i]]++;
+        }
+
+        while(r < n) {
+
+            if(hash[s[r]] > 0) {
+                cnt++;
+            }
+
+            hash[s[r]]--;
+
+            while(cnt == m) {
+
+                if(r - l + 1 < minLen) {
+                    minLen = r - l + 1;
+                    sIndex = l;
+                }
+
+                hash[s[l]]++;
+
+                if(hash[s[l]] > 0) {
+                    cnt--;
+                }
+
+                l++;
+            }
+
+            r++;
+        }
+
+        if(sIndex == -1) {
+            return "";
+        }
+
+        return s.substr(sIndex, minLen);
+    }
+};
