0f8a277a176f7f1140601eac8fb89b6088f33cf9
Gaurav-102005 <gauravbhasker10@gmail.com>
2026-09-22 21:00:08 +0530

    Time: 15 ms (15.27%) | Memory: 44.8 MB (78.37%) - LeetSync

--- leetcode/455-assign-cookies/assign-cookies.cpp
+++ leetcode/455-assign-cookies/assign-cookies.cpp
@@ -5,17 +5,12 @@ public:
         sort(s.begin(), s.end());
         int i = 0;
         int j = 0;
-        int ans = 0;
         while(i < g.size() && j < s.size()) {
             if(s[j] >= g[i]){
-                ans++;
                 i++;
-                j++;
             }
-            else{
-                j++;
-            }
+            j++;
         }
-        return ans;
+        return i;
     }
 };
